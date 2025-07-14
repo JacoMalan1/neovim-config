94a600fe908202e95ad4d46a132e9951c12b5fab
Jaco Malan <jacomalan.12@gmail.com>
2025-07-14 14:04:23 +0200

    Add mason-lspconfig and nvim-cmp

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,6 +1,10 @@
 return {
   { "onsails/lspkind.nvim" },
-  {
-    "hrsh7th/nvim-cmp",
-  }
+  { "hrsh7th/cmp-nvim-lsp" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-buffer" },
+  { "hrsh7th/cmp-path" },
+  { "hrsh7th/cmp-cmdline" },
+  { "saadparwaiz1/cmp_luasnip" },
+  { "hrsh7th/nvim-cmp" },
 }
